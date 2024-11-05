57f5dbac0d811e28f504e3016033317853b2f1fd
Shawlock <132828185+Shawlock2@users.noreply.github.com>
2024-11-05 14:39:20 -0300

    Add files via upload

--- matriz.c
+++ matriz.c
@@ -48,20 +48,86 @@ void imprimeMatriz(int vet[], int l, int c){
     }
 }
 
+void produtoMatriz(int vet1[], int vet2[], int l, int c){
+    int tamanhoMatriz = l*c;
+    int vet3[tamanhoMatriz];
+    for(int i = 1; i <= l; i++){
+        for(int j = 1; j <= c; j++){
+            int valor1 = buscarElemento(vet1,i,j);
+            int valor2 = buscarElemento(vet2,i,j);
+            int valor3 = valor1*valor2;
+            adicionaElemento(vet3,valor3, i, j);
+        }
+    }
+
+    imprimeMatriz(vet3, l, c);
+}
+
+void transpostaSomaMatriz(int vet1[], int vet2[], int l, int c){
+    int tamanhoMatriz = l*c;
+    int vet3[tamanhoMatriz];
+    for(int i = 1; i <= l; i++){
+        for(int j = 1; j <= c; j++){
+            int valor1 = buscarElemento(vet1,i,j);
+            int valor2 = buscarElemento(vet2,i,j);
+            int valor3 = valor1+valor2;
+            adicionaElemento(vet3,valor3, j, i);
+        }
+    }
+
+    imprimeMatriz(vet3, c, l);
+}
+
+int identidadeMatriz(int vet[], int ordem){
+    int saida = 1;
+    int controlador = 1;
+    while(controlador <= ordem && saida == 1){
+        saida = buscarElemento(vet, controlador, controlador);
+        controlador++;
+    }
+
+    if(saida != 1){
+        saida = 0;
+    }
+
+    return saida;
+
+}
+
+void inversaMatriz(int vet1[], int vet2[], int ordem){
+    
+    produtoMatriz(vet1, vet2, ordem, ordem);
+    int resultado = identidadeMatriz(vet3, ordem);
+    if(resultado == 1){
+        printf("A função B é inversa de A\n");
+    }
+    else{
+        printf("A função B não é inversa de A\n");
+    }
+}
 
 void main(){
     dimensionaMatriz(3,3);
     int vet[tam];
+    int vet2[tam];
 
     zeraMatriz(vet);
+    zeraMatriz(vet2);
 
-    imprimeMatriz(vet,linhas,colunas);
+    adicionaElemento(vet,2,1,1);
+    adicionaElemento(vet,2,2,2);
+    adicionaElemento(vet,2,3,3);
 
-    adicionaElemento(vet,15,1,1);
-    adicionaElemento(vet,25,2,2);
-    adicionaElemento(vet,30,3,3);
+    adicionaElemento(vet2,1/2,1,1);
+    adicionaElemento(vet2,1/2,2,2);
+    adicionaElemento(vet2,1/2,3,3);
 
-    imprimeMatriz(vet,linhas,colunas);
+    produtoMatriz(vet, vet2, linhas, colunas);
+    transpostaSomaMatriz(vet, vet2, linhas, colunas);
 
-    printf("O elemento que esta na linha 2 coluna 2 eh: %d \n\n", buscarElemento(vet,2,2));
+    printf("%d\n", identidadeMatriz(vet,linhas));
+
+    inversaMatriz(vet, vet2, linhas);
+
+    // printf("O elemento que esta na linha 2 coluna 2 eh: %d \n\n", buscarElemento(vet,2,2));
 }
