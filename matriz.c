8742494cec5df838dc6c65faa3fa94230b205ce7
Shawlock <132828185+Shawlock2@users.noreply.github.com>
2024-11-05 09:27:06 -0300

    Add files via upload

--- matriz.c
+++ matriz.c
@@ -2,45 +2,52 @@
 
 int tam, linhas, colunas;
 
+//Define o tamanho total da matriz
 void dimensionaMatriz(int l, int c){
     linhas = l;
     colunas = c;
     tam = l*c;
 }
 
-void zeraMatriz(int vet[]){
-    for(int i = 0; i < tam; i++){
-        vet[i] = 0;
-    }
-}
-
-void imprimeMatriz(int vet[], int l, int c){
-    int k = 0;
-    for(int i = 0; i < l; i++){
-        for(int j = 0; j < c; j++){
-            printf("%d ", vet[k]);
-            k++;
-        }
-        printf("\n");
-    }
-}
-
+//Define o indice de um elemento no vetor, simulando uma matriz
 int indice(int linha, int coluna){
-    int k = (linha-1) * (colunas) + (coluna-1);
+    int k = (linha-1) * (colunas) + (coluna-1);//formula para calcular o indice
     return k;
 
 }
 
+//Muda o valor atual de um elemento na matriz
 void adicionaElemento(int vet[], int n, int l, int c){
     int k = indice(l, c);
     vet[k] = n;
 }
 
+//Retorna o valor de um elemento de acordo com seu indice
 int buscarElemento(int vet[], int l, int c){
     int k = indice(l,c);
     int elemento = vet[k];
     return elemento;
 }
+
+//Define todos os valores da matriz como 0
+void zeraMatriz(int vet[]){
+    for(int i = 1; i <= linhas; i++){
+        for(int j = 1; j <= colunas; j++){
+            adicionaElemento(vet, 0, i, j);
+        }
+    }
+}
+
+//Exibe os valores da matriz
+void imprimeMatriz(int vet[], int l, int c){
+    for(int i = 1; i <= l; i++){
+        for(int j = 1; j <= c; j++){
+            printf("%d ", buscarElemento(vet, i, j));
+        }
+        printf("\n");
+    }
+}
+
 
 void main(){
     dimensionaMatriz(3,3);
